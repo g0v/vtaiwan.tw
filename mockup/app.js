6d26ac2c801024f8827a9c72d67beb61b64e1d56
Audrey Tang <audreyt@audreyt.org>
2015-01-06 18:47:16 +0800

    * Always use HTTPS

--- mockup/app.js
+++ mockup/app.js
@@ -70,7 +70,7 @@ app.factory('DataService', function ($http, $q){
 
   DataService.getBookData = function(path){
     var deferred = $q.defer();
-    $http.get('http://' + path + '.vtaiwan.tw/content.json').
+    $http.get('https://' + path + '.vtaiwan.tw/content.json').
         success(function(data, status, headers, config) {
           deferred.resolve(data);
         }).
