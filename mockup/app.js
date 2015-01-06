459b51ca68b6450b7d7477d29e43cc95e7c18490
soidid <soidid@gmail.com>
2015-01-06 22:39:38 +0800

    add get data (get all, and then aggregate) service

--- mockup/app.js
+++ mockup/app.js
@@ -51,11 +51,121 @@ app.config(['$routeProvider','$locationProvider',
 
 app.factory('DataService', function ($http, $q){
 
-  var DataService = {};
-  DataService.getData = function(path){
+  var DataService = {}; 
+  var CachedData;  // get json data, properly merged, and save locally.
+
+  var proposals = [
+      { "title_cht" : "群眾募資", "title_eng" : "crowdfunding", "category_num" : 6},
+      { "title_cht" : "閉鎖型公司", "title_eng" : "closelyheld", "category_num" : 5}
+  ];
+
+  DataService.getCatchedData = function(){
     var deferred = $q.defer();
-    $http.get('data/'+path+'.json').
-    success(function(data, status, headers, config) {
+    if(CachedData){
+        deferred.resolve(CachedData);
+    
+    }else{
+      DataService.getData().then(function (data) {
+        deferred.resolve(data);
+      })
+
+    }
+    return deferred.promise;
+  };
+
+  DataService.getData = function(){
+    CachedData = {};
+    var deferred = $q.defer();
+    
+    //[1] Get gitbook content
+    proposals.map(function (proposal_item) {
+        DataService.getBookData(proposal_item.title_eng).then(function (book_data){
+            CachedData[proposal_item.title_eng] = book_data;
+
+            //[2] Add topic id step1: get a list of all topics and ids
+            DataService.getPostIdData(proposal_item.category_num).then(function (id_data){
+                var topics_array = id_data.topic_list.topics;
+                var topics_to_id_table = {};//e.g. topics_to_id_table['群眾募資'] = 14;
+
+                topics_array.map(function (topics_item){
+                    topics_to_id_table[topics_item.fancy_title] = topics_item.id;
+                });
+
+                //Add topic id step2: using fancy_title comparason to add id
+                CachedData[proposal_item.title_eng].map(function (category_item){
+
+                    //category_item.children are the discuss topics
+                    if(category_item.children){
+
+                        category_item.children.map(function (children_item) {
+                            children_item.id = topics_to_id_table[children_item.title];
+                        
+                            //[3] Get discuss data from discourse
+                            DataService.getPostData(children_item.id).then(function (posts_data) {
+                                children_item.posts = posts_data.post_stream.posts;
+                                children_item.post_count = posts_data.posts_count;
+                                
+                                // Parse direct image url
+                                // from: "/user_avatar/talk.vtaiwan.tw/audreyt/{size}/6.png"
+                                // to: "/user_avatar/talk.vtaiwan.tw/audreyt/50/6.png"
+                                for(var key in children_item.posts){
+                                    children_item.posts[key].avatar_url = 'https://talk.vtaiwan.tw/user_avatar/talk.vtaiwan.tw/'+children_item.posts[key].username+'/50/'+children_item.posts[key].uploaded_avatar_id+'.png';
+                                }
+
+                                //console.log(CachedData);//debug check
+
+                                //Detect if all data is collected
+                                // (1) last proposal
+                                if(proposal_item.title_eng === proposals[proposals.length-1].title_eng){
+                                    // (2) last proposal's last category
+                                    var categories = CachedData[proposal_item.title_eng];
+                                    if(category_item.title === categories[categories.length-1].title){
+                                        
+                                        // (3) last proposal's last category's last children(topic)
+                                        var topics = category_item.children;
+                                        if(children_item.title === topics[topics.length-1].title){
+                                            //console.log("back!(1)");
+                                            deferred.resolve(CachedData);
+                                        }
+                                    }
+                                }
+                                
+
+                            });
+
+                        });
+                    }else{
+                        //Detect if all data is collected
+                        // (1) last proposal
+                        if(proposal_item.title_eng === proposals[proposals.length-1].title_eng){
+                            // (2) last proposal's last category
+                            var categories = CachedData[proposal_item.title_eng];
+                            if(category_item.title === categories[categories.length-1].title){
+                                console.log("back!(2)");
+                                deferred.resolve(CachedData);
+                                
+                            }
+                        }
+
+
+                    }
+                    
+                });
+
+            });
+
+        });
+        
+    });
+
+    
+    return deferred.promise;
+  };
+
+  DataService.getBookData = function(path){
+    var deferred = $q.defer();
+    $http.get('http://' + path + '.vtaiwan.tw/content.json').
+        success(function(data, status, headers, config) {
           deferred.resolve(data);
         }).
         error(function(data, status, headers, config) {
@@ -64,10 +174,10 @@ app.factory('DataService', function ($http, $q){
     return deferred.promise;
   };
 
-  DataService.getBookData = function(path){
+  DataService.getPostIdData = function(category_num){
     var deferred = $q.defer();
-    $http.get('http://' + path + '.vtaiwan.tw/content.json').
-        success(function(data, status, headers, config) {
+    $http.get('https://cors.vtaiwan.tw/c/'+category_num+'-category.json').
+    success(function(data, status, headers, config) {
           deferred.resolve(data);
         }).
         error(function(data, status, headers, config) {
@@ -88,22 +198,11 @@ app.factory('DataService', function ($http, $q){
     return deferred.promise;
   };
 
-  DataService.getPostID = function(category_num){
-    var deferred = $q.defer();
-    $http.get('https://cors.vtaiwan.tw/c/'+category_num+'-category.json').
-    success(function(data, status, headers, config) {
-          deferred.resolve(data);
-        }).
-        error(function(data, status, headers, config) {
-          deferred.resolve(data);
-        });
-    return deferred.promise;
-  };
-
   return DataService;
 })
 
 app.controller('AuthCtrl',['$scope', 'DataService', '$location', function($scope, DataService, $location){
+  
   $scope.login = function(){
 
     $scope.user = {"name" : "username tool ongcanno tshowall"};
@@ -162,8 +261,8 @@ app.controller('NavCtrl', ['$scope', 'DataService', '$location', '$sce', functio
   
 
   $scope.setTopic = function (value) {
-    console.log(value);
-    console.log("***");
+    //console.log(value);
+    //console.log("***");
     DataService.getBookData('crowdfunding').then(function(crowdfunding_data){
         DataService.getBookData('closelyheld').then(function(closelyheld_data){
         
