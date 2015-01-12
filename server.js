37d5176473d1690ae7636f879c5a39ac12e9ade5
Audrey Tang <audreyt@audreyt.org>
2015-01-12 12:04:40 +0800

    * Global replace

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/', function (req, res) {
   var protocol = (process.env.USE_HTTPS)? 'https://' : 'http://';
 
   res.type('text/html; charset=UTF-8');
-  res.send(html.replace('{{base}}', protocol + req.headers.host));
+  res.send(html.replace(/\{\{base\}\}/g, protocol + req.headers.host));
 
 });
 
