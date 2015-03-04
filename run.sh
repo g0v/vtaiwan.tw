6545391690afd42392dd544177bfed1082b24a80
billy3321 <billy3321@gmail.com>
2015-03-04 15:14:58 +0800

    fix git output

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 env USE_HTTPS=1 forever server.js &
 
 while true; do
-  if $(git pull | grep -q '\->\ origin\/master');then
+  if $(git pull 2>&1 | grep -q '\->\ origin\/master');then
     npm install && gulp
   fi
   sleep 60
