480a918a2eaded1e05232e8f39754fc5b61a0f6e
billy3321 <billy3321@gmail.com>
2015-03-04 23:35:20 +0800

    fix run.sh, hope can avoid use old content bug

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 env USE_HTTPS=1 forever server.js &
 
 while true; do
   if $(git pull 2>&1 | grep -q '\->\ origin\/master');then
-    npm install && gulp
+    npm install && forever stopall && gulp
+    env USE_HTTPS=1 forever server.js &
   fi
   sleep 60
 done
